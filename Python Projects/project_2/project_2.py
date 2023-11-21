4443d62d8054748020ffb077a6b8a91b7f25cffd
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2023-11-21 09:03:20 +0600

    python project

---
+++ Python Projects/project_2/project_2.py
@@ -0,0 +1,10 @@
+# project name - madlibs
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and jump like you are {famous_person}!"
+
+print(madlib)
