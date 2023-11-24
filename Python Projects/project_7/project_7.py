3623548c9efa87bd7c286345fbd3d50da7510f4a
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2023-11-24 06:24:45 +0600

    python project

--- Python Projects/project_7/project_7.py
+++ Python Projects/project_7/project_7.py
@@ -1,5 +1,8 @@
 # Project name - Tic-Tac-Toe
 
+import time
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)] # single list to rep 3x3 board
@@ -37,6 +40,28 @@ class TicTacToe:
                 self.current_winner = letter
             return True
         return False
+    
+    def winner(self, square, letter):
+        row_ind = square // 3
+        row = self.board[row_ind*3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+        
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+        
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+        
+        return False
+        
 
 def play(game, x_player, o_player, print_game=True):
     if print_game:
@@ -58,10 +83,18 @@ def play(game, x_player, o_player, print_game=True):
             
             if game.current_winner:
                 if print_game:
-                    print(letter = 'wins!')
+                    print(letter + ' wins!')
                 return letter
             
             letter = 'O' if letter == 'X' else 'X'
         
-        if print_game:
-            print('It\'s a tie!')
+        time.sleep(0.8)
+        
+    if print_game:
+        print('It\'s a tie!')
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
