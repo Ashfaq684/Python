2406831e5ffff422042c7f3c7e00eace4c248fe9
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2023-11-29 23:54:27 +0600

    python project

--- Python Projects/project_10/project_10.py
+++ Python Projects/project_10/project_10.py
@@ -1,2 +1,13 @@
 # project name - weather app
 
+import requests
+import json
+
+city = input("Enter the name of the city\n")
+
+url = f"https://api.weatherapi.com/v1/current.json?key=b13989793f184149a91141538230103&q={city}"
+
+r = requests.get(url)
+# print(r.text)
+wdic = json.loads(r.text)
+print(wdic["current"]["temp_c"])
