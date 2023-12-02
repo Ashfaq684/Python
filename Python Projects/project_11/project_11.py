c8abf0959f668bff68512724cc30a3cab1b76e6f
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2023-12-02 23:47:31 +0600

    python project

--- Python Projects/project_11/project_11.py
+++ Python Projects/project_11/project_11.py
@@ -2,4 +2,17 @@
 
 import cv2
 
-image = cv2.imread()
+src = cv2.imread("Rintaro.jpg", cv2.IMREAD_UNCHANGED)
+cv2.imshow("title", src)
+
+scale_percent = 50
+
+width = int(src.shape[1] * scale_percent / 100)
+height = int(src.shape[0] * scale_percent / 100)
+
+dsize = (width, height)
+
+output = cv2.resize(src, dsize)
+
+cv2.imwrite('newImage.png', output)
+cv2.waitKey(0)
